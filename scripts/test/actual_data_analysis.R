f7d39c46979f0ea70e8cc105ae14475a69b7408f
yuhan zhu <yuhan.zhu@uq.net.au>
2023-10-04 14:46:28 +1000

    add test scripts

---
+++ scripts/test/actual_data_analysis.R
@@ -0,0 +1,23 @@
+
+S <-as.integer(10^seq(5, 9, by = 0.25))
+S<- as.numeric(S)
+# Create an empty vector to store the median values
+save_median <- numeric(length(S))
+
+# Loop through the values in 'n'
+for (i in 1:length(S)) {
+  # Find the first row number where summary$n == n[i]
+  rownumber1 <- min(which(summary$K == S[i]))
+  
+  # Find the last row number where summary$n == n[i]
+  rownumber2 <- max(which(summary$K == S[i]))
+  
+  # Subset the data frame to rows where summary$n == n[i]
+  subset_data <- summary[rownumber1:rownumber2, ]
+  
+  # Calculate the median of the 'ed_average_without_wildtype' column for this subset
+  save_median[i] <- median(subset_data$sims_with_mutations)
+}
+
+# 'save_median' now contains the median values for each subset 
+save_median
---
+++ scripts/test/ideal_n_Bc.R
@@ -0,0 +1,25 @@
+# Define the number of dimensions and points
+num_dimensions <- 2^(0:7)
+num_points <- num_dimensions
+
+for (k in 1:length(num_points)){
+# Initialize an empty matrix to store points
+points_matrix <- diag(1, nrow = num_points[k], ncol = num_dimensions[k])
+
+# Initialize an empty matrix to store distances
+distance_matrix <- matrix(NA, nrow = num_points[k], ncol = num_points[k])
+
+# Calculate the Bray-Curtis distance between all combinations
+for (i in 1:num_points[k]) {
+  for (j in 1:num_points[k]) {
+      numerator <- sum(abs(points_matrix[i,] - points_matrix[j,]))
+      denominator <- sum(abs(points_matrix[i,] + points_matrix[j,]))
+      distance_matrix[i, j] <- numerator / denominator
+    
+   }
+ }
+# Calculate the average distance
+average_distance[k] <- sum(distance_matrix) / (num_points[k] * num_points[k] )
+}
+# Print the average distance
+print(average_distance)
