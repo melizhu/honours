df67e923f0dea49f413bf5cbba0fffea2f9cc74c
yuhan zhu <yuhan.zhu@uq.net.au>
2023-10-25 14:08:01 +1000

    edit a few comments in n_screen.R

--- scripts/n_screen/n_screen.R
+++ scripts/n_screen/n_screen.R
@@ -12,16 +12,16 @@ source("scripts/NEW_ALL_functions.R")
 n <- 2^(0:7)
 
 # parameters:
-K <- 10^6
-A <- 1
-miu <- 10^(-7)
-Sini <- K
+K <- 10^6 # carrying capacity
+A <- 1 # antibiotic concentration
+miu <- 10^(-7) # mutation rate 
+Sini <- K # initial population
 
-psi <- 0.7
-phi <- 5.0
-kappa <- 2.5
-zeta_shape <- 2
-zeta_scale <- 5
+psi <- 0.7 # growth rate in the absence of antibiotics 
+phi <- 5.0  # maximum reduction in fitness
+kappa <- 2.5 # slope
+zeta_shape <- 2 # random gamma distribution shape for generating mutation MIC
+zeta_scale <- 5 # random gamma distribution scale for generating mutation MIC
 
 # Initialize a list to store the data frames
 sims <- list()
@@ -32,8 +32,10 @@ m <- 1000
 rseed <- sample(1:1e6, 1)
 set.seed(rseed)
 
+#write loop for generating the simulations
 for (i in 1:length(n)) {
   cat(paste0("Running simulations for n=", n[i], ".\n"))
+  #the zeta values for mutations are random gamma distribution and greater than 1
   zeta_m <- rgamma(n[i], shape = zeta_shape, scale = zeta_scale) + 1
   #The code with building data frame for S and all the mutations
   strains <- data.frame(name = c("S", paste0("M", 1:n[i])),
@@ -41,17 +43,21 @@ for (i in 1:length(n)) {
                         phi = phi,
                         kappa = kappa,
                         zeta = c(1, zeta_m))
+  #build a list for saving simulation inputs and results called sims
   sims[[i]] <- list()
+  #sims strains contain all the PD parameters such as psi, phi, kappa and zeta
   sims[[i]]$strains <- strains
-  sims[[i]]$generalinput <- c(K = K,
+  #sims generalinput contain all the other variables 
+  sims[[i]]$generalinput <- c(K = K, 
                               A = A,
                               miu = miu,
                               Sini = Sini,
                               zeta_shape = zeta_shape,
                               zeta_scale = zeta_scale,
                               rseed = rseed)
-
+  #sims results contain the simulation results 
   sims[[i]]$results <- replicate(m, simulate(strains, K = K, A = A, miu = miu, Sini = K))
 }
 
+#save the simulation results to a file 
 save(sims, file = paste0("output/n_screen/SimResults_n_screen_rep_", batch, ".RData"))
